81e94272a5567f574bf29fdc4f71434cb4acf69a
Mustafa GÜNEŞ <gunes149@gmail.com>
2022-11-05 21:14:47 +0300

    Initial Commit

---
+++ PracticalEnglish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticalEnglish
+//
+//  Created by Mustafa Gunes on 5.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
